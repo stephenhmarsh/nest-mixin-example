4bd481315402eaa3c3ebacc3f43116a6c013d348
Stephen Marsh <stephenhmarsh@users.noreply.github.com>
2023-12-11 13:52:10 -0500

    add error as comments

--- src/app.service.ts
+++ src/app.service.ts
@@ -10,26 +10,19 @@ export class AppService {
   }
 }
 
-//
-// src/app.service.ts:6:3 - error TS2390: Constructor implementation is missing.
+// > nest start
 
-// 6   constructor(readonly catsService: CatsService)
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// src/app.service.ts:6:15 - error TS2369: A parameter property is only allowed in a constructor implementation.
-
-// 6   constructor(readonly catsService: CatsService)
-//                 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // src/app.service.ts:6:37 - error TS2749: 'CatsService' refers to a value, but is being used as a type here. Did you mean 'typeof CatsService'?
 
-// 6   constructor(readonly catsService: CatsService)
+// 6   constructor(readonly catsService: CatsService) { }
 //                                       ~~~~~~~~~~~
 // src/app.service.ts:6:37 - error TS4063: Parameter 'catsService' of constructor from exported class has or is using private name 'CatsService'.
 
-// 6   constructor(readonly catsService: CatsService)
+// 6   constructor(readonly catsService: CatsService) { }
 //                                       ~~~~~~~~~~~
 // src/app.service.ts:6:37 - error TS4063: Parameter 'catsService' of constructor from exported class has or is using private name 'CatsService'.
 
-// 6   constructor(readonly catsService: CatsService)
+// 6   constructor(readonly catsService: CatsService) { }
 //                                       ~~~~~~~~~~~
 
-// Found 5 error(s).
+// Found 3 error(s).
